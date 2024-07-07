c46e8e11886f6e276029185407ab252c773a3ee0
Samuel <samuel.kamawira@student.moringaschool.com>
2024-07-07 10:55:27 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+
+  const employee = { name: 'Sam' };
+  
+  const updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+  console.log(updatedEmployee);
+  console.log(employee); 
+  
+  const destructivelyUpdatedEmployee = destructivelyUpdateEmployeeWithKeyAndValue(employee, 'department', 'Engineering');
+  console.log(destructivelyUpdatedEmployee); 
+  console.log(employee);
+  
